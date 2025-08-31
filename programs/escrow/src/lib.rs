19ad0a7a5b37ceec09eae0a78be8f6c445a47120
Mardiprk <mk1659770@gmail.com>
2025-08-31 01:45:10 +0000

    escrow

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("7ptKBMKwd4CdXWP5NXAoqXzsGxhE5ifmDVqxJMtDP9J5");
 
@@ -6,11 +7,57 @@ declare_id!("7ptKBMKwd4CdXWP5NXAoqXzsGxhE5ifmDVqxJMtDP9J5");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_escrow(
+        ctx: Context<InitializeEscrow>,
+        escrow_id: u64,
+        amount: u64,
+        expiry_time: i64,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn release_escrow(ctx: Context<ReleaseEscrow>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn cancel_escrow(ctx: CancelEscrow) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(escrow_id: u64)]
+pub struct InitializeEscrow<'info> {}
+
+#[derive(Accounts)]
+pub struct ReleaseEscrow<'info> {}
+
+#[derive(Accounts)]
+pub struct CancelEscrow<'info> {}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EscrowAccount {
+    pub escrow_id: u64,
+    pub depositer: Pubkey,
+    pub beneficiary: Pubkey,
+    pub mint: Pubkey,
+    pub amount: u64,
+    pub expiry_time: i64,
+    pub is_completed: bool,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum EscrowError {
+    #[msg("Invalid expiry time")]
+    InvalidExpiryTime,
+    #[msg("Invalid amount")]
+    InvalidAmount,
+    #[msg("Escrow already completed")]
+    EscrowAlreadyCompleted,
+    #[msg("Unauthorized cancellation")]
+    UnauthorizedCancellation,
+    #[msg("Escrow has expired")]
+    EscrowExpired,
+}
